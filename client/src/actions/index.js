3ca086341f61f575c06002f9b1e2d04ca2ff6da5
Eva <evangeline013@gmail.com>
2020-04-28 07:53:16 -0400

    add callback to signup action creator to redirect once sign up successfully, add auth HOC to feature component

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 import { AUTH_USER, AUTH_ERROR } from './types';
 
-export const signup = formProps => async dispatch => {
+export const signup = (formProps, callback) => async dispatch => {
     try {
         const res = await axios.post('http://localhost:3090/signup', formProps);
         dispatch({ type: AUTH_USER, payload: res.data.token });
+        callback();
     } catch(err) {
         dispatch({ type: AUTH_ERROR, payload: 'Email in use' });
     }
